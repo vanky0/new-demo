0f77e291c4f19e38950a5bd9a492a263cf237fa0
vanky0 <vanky0@users.noreply.github.com>
2022-03-10 00:00:00 +0000

    update project structure for better maintainability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -126,4 +126,7 @@ function setupAudioContext() {
         audioCtx = new (window.AudioContext || window.webkitAudioContext)();
         analyser = audioCtx.createAnalyser();
         source = audioCtx.createMediaElementSource(audioSource);
-        
+        
+        // Connect the nodes in the audio graph
+        source.connect(analyser);
+        analyser.connect(audioCtx.destination);
