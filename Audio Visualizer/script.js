32373c0d527f67693069cbdae42f9c2169ae7928
vanky0 <vanky0@users.noreply.github.com>
2022-03-11 00:00:00 +0000

    fix typo in variable names

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -138,4 +138,7 @@ function setupAudioContext() {
         
         // Start the visualization loop
         drawVisualizer(bufferLength, dataArray);
-    }
+    }
+}
+
+// Function to handle the audio file upload
