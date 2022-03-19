ccbe827883281f6bf553d1e17676a8a72c62c102
vanky0 <vanky0@users.noreply.github.com>
2022-03-19 00:00:00 +0000

    optimize loops and conditional checks

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -189,4 +189,7 @@ function drawVisualizer(bufferLength, dataArray) {
     
     for(let i = 0; i < bufferLength; i++) {
         const barHeight = dataArray[i] * 1.5;
-        
+        
+        // Simple color gradient based on bar height
+        const hue = i / bufferLength * 360;
+        canvasCtx.fillStyle = `hsl(${hue}, 100%, 50%)`;
