ae80a213abb8ab01e407e671d10e129ecaff76b5
vanky0 <vanky0@users.noreply.github.com>
2022-03-22 00:00:00 +0000

    improve error handling for better stability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -204,4 +204,7 @@ function drawVisualizer(bufferLength, dataArray) {
 // Ensure the audio context starts on a user gesture
 document.addEventListener('click', () => {
     if (audioCtx && audioCtx.state === 'suspended') {
-        audioCtx.resume();
+        audioCtx.resume();
+    }
+});
+
