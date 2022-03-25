dadd0dcb37a34536fee19cc5919f257a61675fba
vanky0 <vanky0@users.noreply.github.com>
2022-03-25 00:00:00 +0000

    enhance code readability

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -19,4 +19,7 @@ const port = 3000;
 
 // Serve static files from the current directory
 app.use(express.static(path.join(__dirname, '/')));
-
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
