a45a566f10606e3549ef0bea9b98faa581265d2b
vanky0 <vanky0@users.noreply.github.com>
2022-06-16 00:00:00 +0000

    minor UI/UX improvements in project

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -225,4 +225,6 @@ const visualizerCanvas = document.getElementById('visualizer');
 const canvasCtx = visualizerCanvas.getContext('2d');
 
 // --- Web Audio API Components ---
-let audioCtx;
+let audioCtx;
+let analyser;
+let source;
