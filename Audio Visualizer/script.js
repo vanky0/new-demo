8d2c9500437ff3c7a0d1531616c79f7131df58c6
vanky0 <vanky0@users.noreply.github.com>
2022-06-22 00:00:00 +0000

    docs: improve inline comments

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -261,4 +261,6 @@ audioUpload.addEventListener('change', (e) => {
         playPauseBtn.textContent = "Play";
         isPlaying = false;
         
-        // Set up the audio context once a file is loaded
+        // Set up the audio context once a file is loaded
+        setupAudioContext();
+    }
