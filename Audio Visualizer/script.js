074acdb1aa9580c2553d3d88e1ccb31054d4e8aa
vanky0 <vanky0@users.noreply.github.com>
2022-06-24 00:00:00 +0000

    update dependencies and config files

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -273,4 +273,6 @@ playPauseBtn.addEventListener('click', () => {
         playPauseBtn.textContent = "Pause";
         isPlaying = true;
     } else {
-        audioSource.pause();
+        audioSource.pause();
+        playPauseBtn.textContent = "Play";
+        isPlaying = false;
