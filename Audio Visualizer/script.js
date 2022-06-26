cfba18b38b0b00000fd31a20e2a7c2eac0460cfa
vanky0 <vanky0@users.noreply.github.com>
2022-06-26 00:00:00 +0000

    improve project folder organization

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -291,4 +291,6 @@ function drawVisualizer(bufferLength, dataArray) {
     // Request the next frame
     requestAnimationFrame(() => drawVisualizer(bufferLength, dataArray));
     
-    // Get the frequency data
+    // Get the frequency data
+    analyser.getByteFrequencyData(dataArray);
+
