f4c7a2f16d5b553644f6136c2c685a0ee9d2be9f
vanky0 <vanky0@users.noreply.github.com>
2022-06-30 00:00:00 +0000

    update project structure for better maintainability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -313,4 +313,6 @@ function drawVisualizer(bufferLength, dataArray) {
 
 // Ensure the audio context starts on a user gesture
 document.addEventListener('click', () => {
-    if (audioCtx && audioCtx.state === 'suspended') {
+    if (audioCtx && audioCtx.state === 'suspended') {
+        audioCtx.resume();
+    }
