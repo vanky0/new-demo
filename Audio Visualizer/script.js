878708ca2fe477aed100a6ab627114375ed4c22d
vanky0 <vanky0@users.noreply.github.com>
2022-07-02 00:00:00 +0000

    remove redundant code for optimization

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -325,4 +325,6 @@ window.addEventListener('resize', () => {
         const bufferLength = analyser.frequencyBinCount;
         const dataArray = new Uint8Array(bufferLength);
         drawVisualizer(bufferLength, dataArray);
-    }
+    }
+});
+
