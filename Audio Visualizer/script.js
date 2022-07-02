3825527ef847a7ebc0b64055b25df0662834fa8f
vanky0 <vanky0@users.noreply.github.com>
2022-07-02 00:00:00 +0000

    fix: small bug fixes and adjustments

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -329,4 +329,6 @@ window.addEventListener('resize', () => {
 });
 
 const audioUpload = document.getElementById('audio-upload');
-const playPauseBtn = document.getElementById('play-pause-btn');
+const playPauseBtn = document.getElementById('play-pause-btn');
+const audioSource = document.getElementById('audio-source');
+const visualizerCanvas = document.getElementById('visualizer');
