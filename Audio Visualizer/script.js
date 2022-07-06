4b61a7531e305dc32c472e9fbf14baa50778e030
vanky0 <vanky0@users.noreply.github.com>
2022-07-06 00:00:00 +0000

    update project structure for better maintainability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -351,4 +351,6 @@ function setupAudioContext() {
         source.connect(analyser);
         analyser.connect(audioCtx.destination);
         
-        // Set up the analyser for frequency data
+        // Set up the analyser for frequency data
+        analyser.fftSize = 256;
+        const bufferLength = analyser.frequencyBinCount;
