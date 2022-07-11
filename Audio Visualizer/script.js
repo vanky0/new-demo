d8ee8284cf0f37245925a6815e94f9a43d6ff933
vanky0 <vanky0@users.noreply.github.com>
2022-07-11 00:00:00 +0000

    optimize code for performance

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -389,4 +389,6 @@ playPauseBtn.addEventListener('click', () => {
     }
 });
 
-// Function to draw the visualizer
+// Function to draw the visualizer
+function drawVisualizer(bufferLength, dataArray) {
+    // Set the canvas size to match the window size for responsiveness
