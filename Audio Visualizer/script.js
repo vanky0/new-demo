fbf00f083a16cade66ea7cd3ca3b37434274a9f5
vanky0 <vanky0@users.noreply.github.com>
2022-07-12 00:00:00 +0000

    chore: clean up unused code

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -397,4 +397,6 @@ function drawVisualizer(bufferLength, dataArray) {
     
     // Clear the canvas
     canvasCtx.clearRect(0, 0, visualizerCanvas.width, visualizerCanvas.height);
-    
+    
+    // Request the next frame
+    requestAnimationFrame(() => drawVisualizer(bufferLength, dataArray));
