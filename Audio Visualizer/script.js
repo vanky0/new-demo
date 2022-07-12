f0ef5d760e79ee9414351510c950d5e808b384a1
vanky0 <vanky0@users.noreply.github.com>
2022-07-12 00:00:00 +0000

    update dependencies and config files

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -393,4 +393,6 @@ playPauseBtn.addEventListener('click', () => {
 function drawVisualizer(bufferLength, dataArray) {
     // Set the canvas size to match the window size for responsiveness
     visualizerCanvas.width = visualizerCanvas.clientWidth;
-    visualizerCanvas.height = visualizerCanvas.clientHeight;
+    visualizerCanvas.height = visualizerCanvas.clientHeight;
+    
+    // Clear the canvas
