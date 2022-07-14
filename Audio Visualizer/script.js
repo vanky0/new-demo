cc8079cb8f6df6082eea948357f4601d983dae8d
vanky0 <vanky0@users.noreply.github.com>
2022-07-14 00:00:00 +0000

    docs: improve inline comments

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -405,4 +405,6 @@ function drawVisualizer(bufferLength, dataArray) {
     analyser.getByteFrequencyData(dataArray);
 
     const barWidth = (visualizerCanvas.width / bufferLength) * 2;
-    let x = 0;
+    let x = 0;
+    
+    for(let i = 0; i < bufferLength; i++) {
