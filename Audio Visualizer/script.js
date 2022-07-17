5b10cb924a93ec2a00ac6fe2e8dec5c8cef91e9b
vanky0 <vanky0@users.noreply.github.com>
2022-07-17 00:00:00 +0000

    remove redundant code for optimization

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -419,4 +419,6 @@ function drawVisualizer(bufferLength, dataArray) {
         
         x += barWidth + 1; // Add a small gap between bars
     }
-}
+}
+
+// Ensure the audio context starts on a user gesture
