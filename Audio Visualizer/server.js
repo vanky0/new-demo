afd341e34367500a441ef7ff545cec9219c7c271
vanky0 <vanky0@users.noreply.github.com>
2022-07-23 00:00:00 +0000

    update project structure for better maintainability

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -31,4 +31,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the current directory
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname, '/')));
+
