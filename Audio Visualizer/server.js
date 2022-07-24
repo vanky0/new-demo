3839a3875c7f5058900862ec51fe788cb0db42ba
vanky0 <vanky0@users.noreply.github.com>
2022-07-24 00:00:00 +0000

    update dependencies and config files

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -43,4 +43,6 @@ app.listen(port, () => {
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3000;
+
+// Serve static files from the current directory
