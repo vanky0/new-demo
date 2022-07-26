8827b5b55ea852c2f4822f7f14ddc76851670a0b
vanky0 <vanky0@users.noreply.github.com>
2022-07-26 00:00:00 +0000

    fix: adjust function parameters for clarity

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -51,4 +51,5 @@ app.use(express.static(path.join(__dirname, '/')));
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-  console.log('Open this URL in your browser to see the visualizer.');
+  console.log('Open this URL in your browser to see the visualizer.');
+});
